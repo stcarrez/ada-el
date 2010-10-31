22d1066e94d404ca50ef102d376770b1d9ff2495
Stephane Carrez <Stephane.Carrez@gmail.com>
2010-10-31 17:50:08 +0000

    Provide a map of EL objects

---
+++ src/el-objects-maps.ads
@@ -0,0 +1,26 @@
+-----------------------------------------------------------------------
+--  EL.Objects.Maps -- Object maps
+--  Copyright (C) 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Ada.Strings.Unbounded;
+with Ada.Containers.Indefinite_Hashed_Maps;
+with Ada.Strings.Unbounded.Hash;
+package EL.Objects.Maps is new
+     Ada.Containers.Indefinite_Hashed_Maps (Key_Type        => Unbounded_String,
+                                            Element_Type    => Object,
+                                            Hash            => Ada.Strings.Unbounded.Hash,
+                                            Equivalent_Keys => "=");
