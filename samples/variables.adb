ee7e7fd1145a7b16c0a7b43c7670c23fb57c2612
Stephane Carrez <Stephane.Carrez@gmail.com>
2010-11-13 17:19:52 +0000

    Add an example of Value_Expression and call to Set_Value

--- samples/variables.adb
+++ samples/variables.adb
@@ -23,7 +23,6 @@
 with EL.Expressions;
 with EL.Objects;
 with EL.Contexts.Default;
-with EL.Beans;
 with Ada.Text_IO;
 with Bean;
 procedure Variables is
@@ -34,6 +33,7 @@ procedure Variables is
    Bill   : Person_Access := Create_Person ("Bill", "Johnson", 42);
    Ctx    : EL.Contexts.Default.Default_Context;
    E      : EL.Expressions.Expression;
+   VE     : EL.Expressions.Value_Expression;
    Result : EL.Objects.Object;
 
 begin
@@ -42,12 +42,21 @@ begin
    --  Bind the context to 'Joe' and evaluate
    Ctx.Set_Variable ("user", Joe);
    Result := E.Get_Value (Ctx);
-   Ada.Text_IO.Put_Line ("Joe's name is " & EL.Objects.To_String (Result));
+   Ada.Text_IO.Put_Line ("User name is " & EL.Objects.To_String (Result));
 
    --  Bind the context to 'Bill' and evaluate
    Ctx.Set_Variable ("user", Bill);
    Result := E.Get_Value (Ctx);
-   Ada.Text_IO.Put_Line ("Bill's name is " & EL.Objects.To_String (Result));
+   Ada.Text_IO.Put_Line ("User name is " & EL.Objects.To_String (Result));
+
+   --  Create a value expression to change the user firstName property.
+   VE := EL.Expressions.Create_Expression ("#{user.firstName}", Ctx);
+
+   --  Change the user first name by setting the value expression.
+   VE.Set_Value (Context => Ctx, Value => EL.Objects.To_Object (String '("Harold")));
+
+   Result := E.Get_Value (Ctx);
+   Ada.Text_IO.Put_Line ("User name is " & EL.Objects.To_String (Result));
 
    Free (Joe);
    Free (Bill);
