6ca396ab543a1642f36b89ba67ce101025c8648f
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-03-17 22:24:40 +0000

    New generic package to create an EL method bound to a procedure with one 'in' parameter

---
+++ src/el-methods-proc_in.adb
@@ -0,0 +1,95 @@
+-----------------------------------------------------------------------
+--  EL.Methods.Proc_In -- Procedure Binding with 1 in argument
+--  Copyright (C) 2010, 2011, 2012 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+package body EL.Methods.Proc_In is
+
+   use EL.Expressions;
+
+   --  ------------------------------
+   --  Execute the method describe by the method binding object.
+   --  The method signature is:
+   --
+   --   procedure F (Obj   : in out <Bean>;
+   --                Param : in Param1_Type);
+   --
+   --  where <Bean> inherits from <b>Readonly_Bean</b>
+   --  (See <b>Bind</b> package)
+   --
+   --  Raises <b>Invalid_Method</b> if the method referenced by
+   --  the method expression does not exist or does not match
+   --  the signature.
+   --  ------------------------------
+   procedure Execute (Method  : in EL.Expressions.Method_Info;
+                      Param   : in Param1_Type) is
+   begin
+      if Method.Binding = null then
+         raise EL.Expressions.Invalid_Method with "Method not found";
+      end if;
+
+      --  If the binding has the wrong type, we are trying to invoke
+      --  a method with a different signature.
+      if not (Method.Binding.all in Binding'Class) then
+         raise EL.Expressions.Invalid_Method
+         with "Invalid signature for method '" & Method.Binding.Name.all & "'";
+      end if;
+      declare
+         Proxy  : constant Binding_Access := Binding (Method.Binding.all)'Access;
+      begin
+         Proxy.Method (Method.Object, Param);
+      end;
+   end Execute;
+
+   --  ------------------------------
+   --  Execute the method describe by the method expression
+   --  and with the given context.  The method signature is:
+   --
+   --   procedure F (Obj   : in out <Bean>;
+   --                Param : in Param1_Type);
+   --
+   --  where <Bean> inherits from <b>Readonly_Bean</b>
+   --  (See <b>Bind</b> package)
+   --
+   --  Raises <b>Invalid_Method</b> if the method referenced by
+   --  the method expression does not exist or does not match
+   --  the signature.
+   --  ------------------------------
+   procedure Execute (Method  : in EL.Expressions.Method_Expression'Class;
+                      Param   : in Param1_Type;
+                      Context : in EL.Contexts.ELContext'Class) is
+      Info : constant Method_Info := Method.Get_Method_Info (Context);
+   begin
+      Execute (Info, Param);
+   end Execute;
+
+   --  ------------------------------
+   --  Proxy for the binding.
+   --  The proxy declares the binding definition that links
+   --  the name to the function and it implements the necessary
+   --  object conversion to translate the <b>Readonly_Bean</b>
+   --  object to the target object type.
+   --  ------------------------------
+   package body Bind is
+      procedure Method_Access (O  : access Util.Beans.Basic.Readonly_Bean'Class;
+                               P1 : in Param1_Type) is
+         Object : constant access Bean := Bean (O.all)'Access;
+      begin
+         Method (Object.all, P1);
+      end Method_Access;
+   end Bind;
+
+end EL.Methods.Proc_In;
---
+++ src/el-methods-proc_in.ads
@@ -0,0 +1,102 @@
+-----------------------------------------------------------------------
+--  EL.Methods.Proc_in -- Procedure Binding with 1 in argument
+--  Copyright (C) 2010, 2011, 2012 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with EL.Expressions;
+with EL.Contexts;
+with Util.Beans.Methods;
+with Util.Beans.Basic;
+generic
+   type Param1_Type (<>) is limited private;
+package EL.Methods.Proc_In is
+
+   use Util.Beans.Methods;
+
+   --  Execute the method describe by the method expression
+   --  and with the given context.  The method signature is:
+   --
+   --   procedure F (Obj   : in out <Bean>;
+   --                Param : in out Param1_Type);
+   --
+   --  where <Bean> inherits from <b>Readonly_Bean</b>
+   --  (See <b>Bind</b> package)
+   --
+   --  Raises <b>Invalid_Method</b> if the method referenced by
+   --  the method expression does not exist or does not match
+   --  the signature.
+   procedure Execute (Method  : in EL.Expressions.Method_Expression'Class;
+                      Param   : in Param1_Type;
+                      Context : in EL.Contexts.ELContext'Class);
+
+   --  Execute the method describe by the method binding object.
+   --  The method signature is:
+   --
+   --   procedure F (Obj   : in out <Bean>;
+   --                Param : in out Param1_Type);
+   --
+   --  where <Bean> inherits from <b>Readonly_Bean</b>
+   --  (See <b>Bind</b> package)
+   --
+   --  Raises <b>Invalid_Method</b> if the method referenced by
+   --  the method expression does not exist or does not match
+   --  the signature.
+   procedure Execute (Method  : in EL.Expressions.Method_Info;
+                      Param   : in Param1_Type);
+
+   --  Function access to the proxy.
+   type Proxy_Access is
+      access procedure (O : access Util.Beans.Basic.Readonly_Bean'Class;
+                        P : in Param1_Type);
+
+   --  The binding record which links the method name
+   --  to the proxy function.
+   type Binding is new Method_Binding with record
+      Method : Proxy_Access;
+   end record;
+   type Binding_Access is access constant Binding;
+
+   --  Proxy for the binding.
+   --  The proxy declares the binding definition that links
+   --  the name to the function and it implements the necessary
+   --  object conversion to translate the <b>Readonly_Bean</b>
+   --  object to the target object type.
+   generic
+      --  Name of the method (as exposed in the EL expression)
+      Name : String;
+
+      --  The bean type
+      type Bean is new Util.Beans.Basic.Readonly_Bean with private;
+
+      --  The bean method to invoke
+      with procedure Method (O  : in out Bean;
+                             P1 : in Param1_Type);
+   package Bind is
+
+      --  Method that <b>Execute</b> will invoke.
+      procedure Method_Access (O  : access Util.Beans.Basic.Readonly_Bean'Class;
+                               P1 : in Param1_Type);
+
+      F_NAME : aliased constant String := Name;
+
+      --  The proxy binding that can be exposed through
+      --  the <b>Method_Bean</b> interface.
+      Proxy  : aliased constant Binding
+        := Binding '(Name => F_NAME'Access,
+                     Method => Method_Access'Access);
+   end Bind;
+
+end EL.Methods.Proc_In;
