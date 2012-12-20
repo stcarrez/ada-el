5fdf64c1c44fed5d4e0cdfdd6a193f45a1e23933
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-12-20 17:44:39 +0000

    Use the test name EL.Contexts

--- regtests/el-contexts-tests.adb
+++ regtests/el-contexts-tests.adb
@@ -28,7 +28,7 @@ package body EL.Contexts.Tests is
 
    use Util.Tests;
 
-   package Caller is new Util.Test_Caller (Test);
+   package Caller is new Util.Test_Caller (Test, "EL.Contexts");
 
    procedure Add_Tests (Suite : in Util.Tests.Access_Test_Suite) is
    begin
